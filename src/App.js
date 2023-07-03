6334e7e0e088d0f8054216e62669164c270b0c11
nish <nish159@outlook.com>
2023-07-03 12:41:16 -0400

    updating and clean up of application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from './TodoList'
 
 function App() {
-  return null
+  return (
+    <>
+      {/* render the TodoList component */}
+      <TodoList />
+
+      {/* input field for adding new todos */}
+      <input type="text" />
+
+      {/* button to add a new todo */}
+      <button>Add Todo</button>
+
+      {/* button to clear completed todos */}
+      <button>Clear Completed Todos</button>
+
+      {/* display the count of todos left to do */}
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
